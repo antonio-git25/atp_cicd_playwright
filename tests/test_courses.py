48ff2d3f23c69b6459622d947362394b9861205e
antonio-git25 <anton.lutsenko1990@yandex.ru>
2025-10-26 02:03:38 +0300

    component make homework

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,4 +1,3 @@
-from playwright.sync_api import sync_playwright, Page, expect
 import pytest
 
 from pages.courses_list_page import CoursesListPage
@@ -9,31 +8,30 @@ from pages.create_course_page import CreateCoursePage
 @pytest.mark.courses
 def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
     create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
-    create_course_page.check_visible_create_course_title()
-    create_course_page.check_disabled_create_course_button()
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=False) # is_image_uploaded=False
-    create_course_page.check_visible_create_course_form(
+    create_course_page.create_course_toolbar_view.check_visible()
+
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
+    create_course_page.create_course_form.check_visible(
         title="", estimated_time="", description="", max_score="0", min_score="0"
     )
-    create_course_page.check_visible_exercises_title()
-    create_course_page.check_visible_create_exercise_button()
+
+    create_course_page.create_course_exercises_toolbar_view.check_visible()
     create_course_page.check_visible_exercises_empty_view()
 
-    create_course_page.upload_preview_image("./testdata/RW_4.jpg")
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=True) #
-
-    create_course_page.fill_create_course_form(
+    create_course_page.image_upload_widget.upload_preview_image("./testdata/RW_4.jpg")
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+    create_course_page.create_course_form.fill(
         title="Playwright",
         estimated_time="2 weeks",
         description="Playwright",
         max_score="100",
         min_score="10"
     )
-    create_course_page.click_create_course_button()
 
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button() #
+    create_course_page.create_course_toolbar_view.click_create_course_button()
+
+    courses_list_page.toolbar_view.check_visible()
+
     courses_list_page.course_view.check_visible(
         index=0,
         title="Playwright",
@@ -52,9 +50,8 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.navbar.check_visible("username")
     courses_list_page.sidebar.check_visible()
 
-    courses_list_page.check_visible_courses_title()
+    courses_list_page.toolbar_view.check_visible()
     courses_list_page.check_visible_empty_view()
-    courses_list_page.check_visible_create_course_button()
 
 
 
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -9,7 +9,9 @@ from pages.registration_page import RegistrationPage
 @pytest.mark.registration
 def test_successfull_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
     registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.fill_registration_form(email='user.name@gmail.com', username='username', password='password')
+    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
     registration_page.click_registration_button()
     dashboard_page.check_visible_dashboard_title()
 
+
+# python -m pytest -k "test_successfull_registration" -s -v
